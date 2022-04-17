25ff3748b1153a4f2adf5a2f99004e92481b752c
Warren Gates <moikapy@devmoi.com>
2022-04-17 16:59:44 -0400

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,11 +6,13 @@ async function main() {
   const mc = await ERC721.deploy();
 
   await mc.deployed(
-    'insert name', // collection name
-    'symbol', // symbol
+    'collection name', // collection name
+    'collection symbol', // symbol
     10000, // supply
     200, //reserve
-    'uri' //uri'
+    'QmP5NXDTvFmFQiU91xDdt56yfSPybCUb22mX3Zkvg3nJDT', //reveal cid
+    5000000000000000000, // price in wei
+    4000000000000000000 // price in wei
   );
   console.log('YORKIES deployed to:', mc, mc.address);
 }
